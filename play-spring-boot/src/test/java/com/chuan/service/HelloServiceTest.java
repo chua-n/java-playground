3056dceeedce6eed53dd0bb37b81685bbcef1250
xucy-e <xucy-e@glodon.com>
2023-02-07 10:01:42 +0800

    little update

--- play-spring-boot/src/test/java/com/chuan/service/HelloServiceTest.java
+++ play-spring-boot/src/test/java/com/chuan/service/HelloServiceTest.java
@@ -1,12 +1,11 @@
 package com.chuan.service;
 
+import com.chuan.component.RequestScopeTester;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.*;
 
 /**
  * @author xucy-e
@@ -16,9 +15,18 @@ import static org.junit.Assert.*;
 public class HelloServiceTest {
     @Autowired
     private HelloService helloService;
+    @Autowired
+    private RequestScopeTester requestScopeTester;
 
     @Test
     public void testSayHello() {
         helloService.sayHello();
+    }
+
+    @Test
+    public void testRequestScope() {
+        // 这里没有HTTP请求，那产生实际的Bean了没？
+        requestScopeTester.saySomething();
+        requestScopeTester.saySomething();
     }
 }
