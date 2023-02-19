cafa8685609f5dd2fce3861b8473119423ef0ebf
chuan <chuan@cmd.redmibook>
2023-02-19 17:53:28 +0800

    spring-boot添加对application配置文件的优先级把玩代码

--- play-spring-boot/src/main/java/com/chuan/PlaySpringBootApplication.java
+++ play-spring-boot/src/main/java/com/chuan/PlaySpringBootApplication.java
@@ -3,6 +3,7 @@ package com.chuan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MutablePropertySources;
 
 @SpringBootApplication
 public class PlaySpringBootApplication {
@@ -11,6 +12,10 @@ public class PlaySpringBootApplication {
 
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(PlaySpringBootApplication.class, args);
+        MutablePropertySources propertySources = applicationContext.getEnvironment().getPropertySources();
+        // applicationXxx的一系列配置文件都会被SpringBoot读取到Spring容器，只不过其中配置是否生效取决于优先级关系等设定
+        // 查看其中的若干个PropertySource的顺序，就可以知道application配置文件的优先级关系了
+        System.out.println(propertySources);
     }
 
 }
